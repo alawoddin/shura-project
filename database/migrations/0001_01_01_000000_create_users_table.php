34de15fe80bff5715a0ac92abfc051856eb45427
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-18 12:41:43 +0430

    the table is add

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,7 +27,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            // $table->foreign('plan_id')->references('id')->on('plans')->nullOnDelete();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
