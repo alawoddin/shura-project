97b9f07ce1c125bec34532574b38eb14a12ac711
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-21 23:00:43 +0430

    the Income controller is create

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
    protected $guarded = [];
+
+   public function incomes()
+{
+    return $this->hasMany(Income::class);
+}
 }
