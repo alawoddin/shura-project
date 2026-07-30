c98a86ce1a0c51b192302a49aa2f24e0a91dc8bc
elyasdehati <elyas786dehaty@gmail.com>
2026-07-30 11:46:46 +0430

    Search Report by Year page translated

--- resources/views/admin/pages/report/search_by_year.blade.php
+++ resources/views/admin/pages/report/search_by_year.blade.php
@@ -1,62 +1,51 @@
 @extends('admin.dashboard')
-
 @section('admin')
 
 <div class="content">
     <div class="container-xxl">
-
         <div class="py-3">
             <h4>
-                Yearly Financial Report ( {{ $year }} )
+                گزارش مالی سالانه ( {{ $year }} )
             </h4>
         </div>
-
         <div class="row">
-
             <div class="col-md-3">
                 <div class="card border-success">
                     <div class="card-body text-center">
-                        <h5>Total Receive</h5>
+                        <h5>مجموع دریافتی</h5>
                         <h2>{{ number_format($totalReceive) }}</h2>
                     </div>
                 </div>
             </div>
-
             <div class="col-md-3">
                 <div class="card border-info">
                     <div class="card-body text-center">
-                        <h5>Total Income</h5>
+                        <h5>مجموع درآمد</h5>
                         <h2>{{ number_format($totalIncome) }}</h2>
                     </div>
                 </div>
             </div>
-
             <div class="col-md-3">
                 <div class="card border-danger">
                     <div class="card-body text-center">
-                        <h5>Total Expense</h5>
+                        <h5>مجموع مصارف</h5>
                         <h2>{{ number_format($totalExpense) }}</h2>
                     </div>
                 </div>
             </div>
-
             <div class="col-md-3">
                 <div class="card border-primary">
                     <div class="card-body text-center">
-                        <h5>Balance</h5>
+                        <h5>بلانس</h5>
                         <h2>{{ number_format($balance) }}</h2>
                     </div>
                 </div>
             </div>
-
         </div>
-
-        {{-- Expenses Table --}}
-        {{-- Receives Table --}}
-        {{-- Income Table --}}
-
-        {{-- Copy the same three tables from search_by_month.blade.php --}}
-
+        {{-- جدول مصارف --}}
+        {{-- جدول دریافتی --}}
+        {{-- جدول درآمد --}}
+        {{-- همان سه جدول فایل search_by_month.blade.php را اینجا کپی کنید --}}
     </div>
 </div>
 
